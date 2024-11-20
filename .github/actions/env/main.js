a488cf94f1d0db99b7fe1d7e2f96430cf39a0405
duynguyen-s1 <duy.nguyen@system1.com>
2024-11-19 20:47:57 -0800

    Initial set up

---
+++ .github/actions/env/main.js
@@ -0,0 +1,10 @@
+const core = require("@actions/core");
+try {
+  const json = core.getInput("vars", { required: true });
+  const vars = JSON.parse(json);
+  for (const [key, value] of Object.entries(vars)) {
+    core.exportVariable(key, value);
+  }
+} catch (error) {
+  core.setFailed(error.message);
+}
